7caf418ba63ebd66403447ed330ad9214ccf84fe
vinothelango <vinothknp22@gmail.com>
2025-07-17 09:35:32 +0530

    add somes

--- firebase.js
+++ firebase.js
@@ -1,8 +1,11 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const admin = require('firebase-admin');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fir-b80d9.firebaseio.com" 
 });
+
 
 module.exports = admin;
